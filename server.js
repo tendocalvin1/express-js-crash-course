89a847a066fe71e6814983ffe365fad62e708c4e
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-14 10:52:30 +0300

    Learning about file paths in Express JS

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 
 const express = require('express')
+const path = require('path')
+
 
 const app = express();
 
+
 app.get('/', (req, res)=>{
-    res.send('<h2>Hello World</h2>');
+    res.sendFile(path.join(__dirname, 'public','index.html'));
 });
 
 app.get('/about', (req, res)=>{
-    res.send('<h>About Route</h2>');
+    res.sendFile(path.join(__dirname, 'public','about.html'));
 });
 app.listen(8000, ()=> console.log(`Server is running on port 8000`));
 
