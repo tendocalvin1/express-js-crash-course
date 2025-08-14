ef55841a9b08e0a365588b8172a47cc52304ff00
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-14 11:10:47 +0300

    Created some basic APIs using Express JS

--- server.js
+++ server.js
@@ -5,13 +5,29 @@ const path = require('path')
 
 const app = express();
 
+// setting up static folder that is the public folder
+// app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public','index.html'));
-});
+let posts = [
+    {id : 1, title : 'post one'},
+    {id : 2, title : 'post two'},
+    {id : 3, title : 'post three'},
 
-app.get('/about', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public','about.html'));
-});
+]
+
+app.get('/api/posts', (req, res)=>{
+    res.json(posts)
+})
+
+
+// app.get('/', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'public','index.html'));
+// });
+
+// app.get('/about', (req, res)=>{
+//     res.sendFile(path.join(__dirname, 'public','about.html'));
+// });
+
+
 app.listen(8000, ()=> console.log(`Server is running on port 8000`));
 
