ff3efc4b17373e5011079db18baa86f5ac60998d
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2025-08-16 11:49:58 +0300

    form submission to an API

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,6 @@
 const output = document.querySelector('#output');
 const button = document.querySelector('#get-posts-button')
+const form = document.querySelector('#add-post-form')
 
 
 // Get and show Posts
@@ -13,13 +14,49 @@ async function showPosts(){
         const posts = await res.json();
         output.innerHTML = '';
     
-        posts.array.forEach((post) => {
+        posts.forEach((post) => {
             const postEl = document.createElement('div');
             postEl.textContent = post.title;
             output.appendChild(postEl)
         });
     }catch(error){
-
+        console.log('Error fetching posts: ', error)
     }
     
 }
+
+// submit new post
+async function addPost(e){
+    e.preventDefault();
+    const formData = new FormData(this);
+    const title = formData.get('title')
+}
+
+try {
+    const res = await fetch('http://localhost:8000/api/posts', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({title})
+    })
+
+    if(!res.ok){
+        throw new Error('Failed to add a new post')
+    }
+
+    const newPost = await res.json()
+    const postEl = document.createElement('div')
+    postEl.textContent = newPost.title;
+    output.appendChild(postEl);
+    showPosts();
+    
+} catch (error) {
+    console.error('Error adding post');
+}
+
+
+// Event listeners
+button.addEventListener('click', showPosts)
+form.addEventListener('submit', addPost)
